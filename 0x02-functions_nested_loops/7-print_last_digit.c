f8623fdb8633373bba2bcd570163d22443da4867
Eya551 <ayahamdi551@gmail.com>
2023-03-21 07:43:11 -0700

    7. There are only 3 colors, 10 digits, and 7 notes;..

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,8 +5,7 @@
 * Return: value of the last digit
 */
 int print_last_digit(int n)
-{
-int a;	
+{int a;	
 if (n < 0)
 	n = -n;
 a = n % 10;
@@ -14,6 +13,5 @@ if (a < 0)
 	a = -a;
 _putchar(a + '0');
 return (a);
-		
 }
 
